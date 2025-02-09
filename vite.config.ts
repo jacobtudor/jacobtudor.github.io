01e9dba1fd68e913586d5b62e2cc2fc0065fede4
jacobtudor <jtudor.141@gmail.com>
2025-02-09 17:08:26 +0000

    Base in Vite config added, Git repo name, dist built

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/jacob-bioinformatics-development-website/',
   plugins: [
     vue(),
     vueDevTools(),
