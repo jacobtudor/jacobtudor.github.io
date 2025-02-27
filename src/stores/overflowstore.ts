57cff6c1e13f80dbf053d003d0cc7eee27d62505
jacobtudor <jtudor.141@gmail.com>
2025-02-27 21:31:12 +0000

    dist

--- src/stores/overflowstore.ts
+++
@@ -1,9 +0,0 @@
-// src/stores/overflowStore.js
-import { reactive } from 'vue'
-
-export const overflowState = reactive({
-  isOverflowHidden: false,
-  toggleOverflow() {
-    this.isOverflowHidden = !this.isOverflowHidden
-  }
-})
