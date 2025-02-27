dca52fe6e6ef1df6e8d59ab7c6c09181cc913a44
jacobtudor <jtudor.141@gmail.com>
2025-02-27 18:51:41 +0000

    vite config change

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production'
-  ? '/jacobtudor.github.io' // Set base path for production
+  ? 'jacobtudor.github.io' // Set base path for production
   : '/', // Set base path for development
   plugins: [
     vue(),
