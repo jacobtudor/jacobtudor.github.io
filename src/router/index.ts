e71461bb76bed4a4bd9829525dfed9df4b4b6a8f
jacobtudor <jtudor.141@gmail.com>
2025-02-27 20:43:31 +0000

    remove comments

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,3 +22,28 @@ const router = createRouter({
 
 export default router
 
+// {
+    //   path: '/articles',
+    //   name: 'articles',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/ArticlesView.vue')
+    // },
+    // {
+    //   path: '/projects',
+    //   name: 'projects',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/ArticlesView.vue')
+    // },
+    // {
+    //   path: '/article/:id',
+    //   name: 'article',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/ArticleView.vue'),
+    //   props: true
+    // },
